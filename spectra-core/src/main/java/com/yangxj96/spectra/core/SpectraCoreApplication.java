25ff043bda762cd84aebd391d04cc78c2e51fb58
yangxj96 <yangxj96@gmail.com>
2025-03-21 15:25:32 +0800

    feat: 初始化

---
+++ spectra-core/src/main/java/com/yangxj96/spectra/core/SpectraCoreApplication.java
@@ -0,0 +1,13 @@
+package com.yangxj96.spectra.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectraCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpectraCoreApplication.class, args);
+    }
+
+}
---
+++ spectra-web/src/main/java/com/yangxj96/spectra/web/SpectraApplication.java
@@ -0,0 +1,19 @@
+package com.yangxj96.spectra.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author 杨新杰
+ * @since 2025/3/21 15:20
+ */
+@SpringBootApplication
+public class SpectraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpectraApplication.class, args);
+    }
+
+}
